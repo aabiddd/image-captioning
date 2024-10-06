0ce0febf50c03801d1106340c696bba1530b996e
Aabid <aabeedalee@gmail.com>
2024-10-06 14:25:45 +0545

    Implement MongoDB to store chat history

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# streamlit run app.py --server.enableXsrfProtection false
 import os
 import time
 import utils
@@ -6,18 +7,22 @@ from PIL import Image
 import streamlit as st
 from io import BytesIO
 
+# Streamed response emulator
 def response_generator(prompt: str, validness: bool):
+    # if given prompt is a valid URL
     if validness:
-        response = "This is some caption, that will get replaced by the model."
-        for word in response.split():
-            yield word + " "
-            time.sleep(0.1)
-    else:
-        response = "Given URL is Invalid. Please Input a Valid URL."
+        response = "This is some caption, that will get replaced by the model." 
         for word in response.split():
             yield word + " "
             time.sleep(0.1)
 
+    else:
+        response = "Given URL is Invalid. Please Input a Valid URL." 
+        for word in response.split():
+            yield word + " "
+            time.sleep(0.1)
+
+# get image via requests
 def get_image(img_url: str):
     img = None        
     try:
@@ -27,88 +32,133 @@ def get_image(img_url: str):
         st.error(f"Failed to load image from URL. \nError: {e}")
     return img
 
+# save chat history based on current session key
 def save_chat_history():
-    if st.session_state.messages:
-        session_key = st.session_state.session_key
-        utils.save_chat_history_to_db(session_key, st.session_state.messages)
-
+    # Check if there are any messages to save
+    if st.session_state.messages != []:
+        if st.session_state.session_key == "New Session":
+            # generate new session key based on current time
+            st.session_state.new_session_key = utils.get_timestamp()
+            utils.save_chat_history_mongo(st.session_state.new_session_key, st.session_state.messages)
+        else:
+            utils.save_chat_history_mongo(st.session_state.session_key, st.session_state.messages)
+        
 def track_index():
     st.session_state.session_index_tracker = st.session_state.session_key
 
 def main():
+    # title
     st.title("ICRT")
-    st.markdown('''### An Integrative Approach To Image Captioning with ResNet and Transformers''')
+    st.markdown('''
+        ### An Integrative Approach To Image Captioning with ResNet and Transformers
+    ''')
 
+    # chat  session sidebar
     st.sidebar.title("Chat Sessions")
-    chat_sessions = ["New Session"] + utils.get_session_list()
 
-    # used a temporary variable, ps only update session key if it differs from the current value 
+    #  List of sessions that will be displayed in the selectbox
+    chat_sessions = ["New Session"] + utils.get_all_sessions()
+
+    # Initialize chat history
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [] # empty history
         st.session_state.session_key = "New Session"
+        st.session_state.new_session_key = None
         st.session_state.session_index_tracker = "New Session"
 
+    # Initialize a counter in the session state if it doesnt' exist
     if 'uploader_reset_counter' not in st.session_state:
         st.session_state.uploader_reset_counter = 0
 
-    # Determine the index of the selected session in the sidebar
+    # save the contents of new_session based on the new_session_key
+    if st.session_state.session_key == "New Session" and st.session_state.new_session_key != None:
+        st.session_state.session_index_tracker = st.session_state.new_session_key
+        st.session_state.new_session_key = None
+
+    # track the index of current session
     index = chat_sessions.index(st.session_state.session_index_tracker)
 
-    # Selectbox for choosing the session
+    # display chat sessions inside the sidebar
     with st.sidebar:
-        selected_session = st.selectbox("Select Chat Session", chat_sessions, index=index, on_change=track_index)
+        selectbox = st.selectbox("Select Chat Session", chat_sessions, key="session_key", index=index, on_change=track_index)
 
-    # Update session state based on selection in the sidebar
-    if selected_session != st.session_state.session_key:
-        st.session_state.session_key = selected_session
-        if selected_session == "New Session":
-            st.session_state.messages = []
-        else:
-            st.session_state.messages = utils.load_chat_history_from_db(selected_session)
+    # load chat history when old session is selected
+    if st.session_state.session_key != "New Session":
+        st.session_state.messages = utils.load_chat_history_mongo(st.session_state.session_key)
+    else:
+        st.session_state.messages = []
 
+
+    # Padding
     for _ in range(15):
         st.sidebar.text("")
 
+    # Generate a unique key for the file uploader widget using the counter
     unique_uploader_key = f"file_uploader_{st.session_state.uploader_reset_counter}"
+
+    # upload local image via file uploader widget with a unique key
     upload_image = st.sidebar.file_uploader("...Or upload a Local Image", type=["png", "jpg", "jpeg"], key=unique_uploader_key)
 
+    # Display chat messages from history on app rerun
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
+            # user message (images)
             if message["role"] == "user":
+                # url-based
                 if message["type"] == "url":
                     img = get_image(message["content"])
                     if img:
                         st.image(img, width=400)
                     else:
+                        # if invalid URL, display the url user entered
                         st.markdown(message["content"])
+                
+                # upload-based
                 elif message["type"] == "upload":
                     img = utils.base64_to_image(message["content"])
                     st.image(img, width=400)
+            
+            # display bot responses
             else:
                 st.markdown(message["content"])
 
-    # Handle new inputs
+    # Accept user input
     if (prompt := st.chat_input("Input Image URL")) or upload_image:
+        # if prompt was given
         if prompt:
-            st.session_state.messages.append({"role": "user", "content": prompt, "type": "url"})
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt, "type":"url"})
+
+            # get image based on user's input url
             img = get_image(prompt)
 
+            # when valid image-url is given, img will not be None, else None
             if img:
+                # Display user input image in chat message container
                 with st.chat_message("user"):
                     st.image(img, width=400)
 
+                # Display assistant response in chat message container
                 with st.chat_message("assistant"):
                     response = st.write_stream(response_generator(prompt, True))
-                st.session_state.messages.append({"role": "assistant", "content": response, "type": "url"})
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response, "type":"url"})
+
             else:
+                # Display user message in chat message container
                 with st.chat_message("user"):
                     st.markdown(prompt)
 
+                # Display assistant response in chat message container
                 with st.chat_message("assistant"):
                     response = st.write_stream(response_generator(prompt, False))
+                # Add assistant response to chat history
                 st.session_state.messages.append({"role": "assistant", "content": response, "type": "url"})
+
+        # else if, image was "uploaded"
         elif upload_image:
             img = Image.open(upload_image)
+            # decode the image into string, so that it becomes JSON serializable
             img_base64 = utils.image_to_base64(img)
 
             with st.chat_message("user"):
@@ -116,17 +166,14 @@ def main():
                 st.session_state.messages.append({"role": "user", "content": img_base64, "type": "upload"})
 
             with st.chat_message("assistant"):
-                response = st.write_stream(response_generator("", True))
+                response = st.write_stream(response_generator(prompt, True))
                 st.session_state.messages.append({"role": "assistant", "content": response, "type": "upload"})
 
+            # the image date gets saved into this variable, due to which the image gets shown on any other sessions (including new session)
+            # Increment the counter to change the key, effectively resetting the file uploader
             st.session_state.uploader_reset_counter += 1
 
-    # If it's a new session, generate a unique session key
-    if st.session_state.session_key == "New Session" and st.session_state.messages:
-        new_session_key = utils.get_timestamp()
-        st.session_state.session_key = new_session_key
-        st.session_state.session_index_tracker = new_session_key
-
+    # save chat history before each app rerun
     save_chat_history()
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,91 +1,53 @@
-import json
 import base64
-import sqlite3
-from io import BytesIO
 from PIL import Image
+from io import BytesIO
 from datetime import datetime
-import os
+from pymongo import MongoClient
+from dotenv import connection_string
 
-# Function to get current timestamp
+def get_db_connection():
+    # cloud based server
+    client = MongoClient(connection_string);
+    db = client["caption_db"]
+    return db
+
+def save_chat_history_mongo(session_key, chat_history):
+    db = get_db_connection()
+    collection = db["caption_db"]
+
+    # Insert a new document or update existing one
+    collection.update_one(
+        {"session_key": session_key}, # Match document by session_key
+        {"$set": {"messages": chat_history, "timestamp": get_timestamp()}}, # update or set messages
+        upsert= True # Create new document if one does not exist
+    )
+
+def load_chat_history_mongo(session_key):
+    db = get_db_connection()
+    collection = db["caption_db"]
+    document = collection.find_one({"session_key": session_key})
+    if document:
+        return document["messages"]
+    else:
+        return []
+    
+def get_all_sessions():
+    db = get_db_connection()
+    collection = db["caption_db"]
+
+    # sort by timestamp field in descending order to get most recent sessions at t op
+    sessions = collection.find({}, {"session_key": 1, "_id": 0}).sort("timestamp", -1) # Fetch only session keys
+    session_list = [session["session_key"] for session in sessions]
+    return session_list
+
 def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Function to convert image to base64 string
 def image_to_base64(image: Image.Image) -> str:
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-# Function to convert base64 string back to image
 def base64_to_image(base64_str: str) -> Image.Image:
     decoded = base64.b64decode(base64_str)
     return Image.open(BytesIO(decoded))
-
-# Function to save chat history to the database
-def save_chat_history_to_db(session_id, messages):
-    conn = sqlite3.connect('chat_history.db')
-    cursor = conn.cursor()
-
-    # Create table if it doesn't exist
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS chat_history (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            session_id TEXT,
-            role TEXT,
-            content TEXT,
-            message_type TEXT,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
-    
-    for message in messages:
-        cursor.execute('''
-            INSERT INTO chat_history (session_id, role, content, message_type)
-            VALUES(?, ?, ?, ?)
-        ''', (session_id, message['role'], message['content'], message['type']))
-
-    conn.commit()
-    conn.close()
-
-# Function to load chat history from the database
-def load_chat_history_from_db(session_id):
-    conn = sqlite3.connect('chat_history.db')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        SELECT role, content, message_type FROM chat_history
-        WHERE session_id = ?
-        ORDER BY timestamp ASC
-    ''', (session_id, ))
-
-    rows = cursor.fetchall()
-
-    messages = [{'role': row[0], 'content': row[1], 'type': row[2]} for row in rows]
-
-    conn.close()
-    return messages
-
-# Function to get a list of distinct session IDs from the database
-def get_session_list():
-    conn = sqlite3.connect('chat_history.db')
-    cursor = conn.cursor()
-
-    cursor.execute('''
-        SELECT DISTINCT session_id FROM chat_history
-    ''')
-    
-    sessions = cursor.fetchall()
-
-    # Convert the list of tuples to a simple list of session IDs
-    session_list = [session[0] for session in sessions]
-
-    conn.close()
-    return session_list
-
-# Function to save chat history as a JSON file
-def save_chat_history_json(messages, file_path):
-    try:
-        with open(file_path, 'w') as json_file:
-            json.dump(messages, json_file, indent=4)
-    except Exception as e:
-        print(f"Error saving chat history to JSON: {e}")
